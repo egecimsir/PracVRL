ade355dda83b0c23fcbcd9cf88c6a731a8ce5f0f
Ege Cimsir <egecimsir99@gmail.com>
2025-07-16 07:57:58 +0200

    fixes and refactoring

--- feature_extractor.py
+++ feature_extractor.py
@@ -42,7 +42,7 @@ class DDTWrapper:
         self.conditioner: BaseConditioner = None
         self.ddt: DDT = None
         self.diff_trainer: BaseTrainer = None
-        self.diffusion_sampler: BaseSampler = None
+        self.sampler: BaseSampler = None
         self.scheduler: LinearScheduler = None
 
         ## Initialization
@@ -60,10 +60,10 @@ class DDTWrapper:
         if self.device == "cuda":
             ckpt = torch.load(ckpt_path)
         else:
-            ckpt = torch.load(ckpt_path, map_location=device, weights_only=True)
+            ckpt = torch.load(ckpt_path, map_location=self.device, weights_only=True)
 
-        self.ddt = load_weights(self.ddt, ckpt).to(device).eval()
-        self.vae = self.vae.to(device).eval()
+        self.ddt = load_weights(self.ddt, ckpt).to(self.device).eval()
+        self.vae = self.vae.to(self.device).eval()
 
         ## Create scheduler
         print("Initializing diffusion trainer, sampler, scheduler")
@@ -84,6 +84,7 @@ class DDTWrapper:
             "init_args": {**cfg["model"]["diffusion_sampler"]["init_args"], "scheduler": self.scheduler}
         }
         self.sampler = instantiate_from_config(sampler_cfg)
+        print(f"Number of encoder blocks: {model.num_encoder_blocks}")
         print("\nCompleted!\n---------------\n")
         
 
@@ -91,37 +92,35 @@ class DDTWrapper:
         assert len(activations) == 0, "Input list must be empty!\n"
 
         def hook_fn(layer: DDTBlock, input: Tensor, output: Tensor):
-            activations.append(output.detach().item())    
+            activations.append(output.detach().cpu().clone())    
         
         ## Register hooks to target layer
         for n, block in self.ddt.blocks.named_children():  ## 0-27
             if n == str(self.num_encoder_blocks-1):
                 block.register_forward_hook(hook_fn)
-                print(f"Num. encoder blocks = {model.num_encoder_blocks}")
                 print(f"Hook registered for layer: ddt.blocks.{n}")
         
 
 
 def extract_features(
-        vae: nn.Module,                 ## Latent space encoder
-        ddt: nn.Module,                 ## Diffusion Transformer
+        vae: nn.Module,
+        ddt: nn.Module,
         scheduler: LinearScheduler,
         time_steps: iter,
         loader: DataLoader,
         device: str, 
-        n_samples=50_000,
-        n_classes=1000,
+        n_samples: int,
+        n_classes: int,
         one_batch=True
     ):
     n_batches = n_samples // loader.batch_size
-    
+
     vae = vae.eval().to(device)
     ddt = ddt.eval().to(device)
 
-    ## Extraction loop
     for t_step in time_steps:
         with torch.no_grad():
-            for i, (img_b, y_b) in enumerate(tqdm(loader, desc="Extracting Features: ", total=n_batches)):
+            for _, (img_b, y_b) in enumerate(tqdm(loader, desc="Extracting Features", total=n_batches)):
                 img_b, y_b = img_b.to(device), y_b.to(device)
 
                 z_raw = vae.encode(img_b)
@@ -130,35 +129,32 @@ def extract_features(
                 noise = torch.rand_like(z)
                 uncond = torch.full_like(y_b, n_classes)
 
-                t = torch.full((z.shape[0],), t_step, device=device).to(device)
-                a_t = scheduler.alpha(torch.tensor([t_step])).view(-1,1,1,1).to(device)
-                s_t = scheduler.sigma(torch.tensor([t_step])).view(-1,1,1,1).to(device)
+                t = torch.full((z.shape[0],), t_step, device=device)
+                a_t = scheduler.alpha(t_step).view(-1, 1, 1, 1).to(device)
+                s_t = scheduler.sigma(t_step).view(-1, 1, 1, 1).to(device)
                 z_t = a_t * z + s_t * noise
-                
-                # No need to reshape - keep the 4D format (B, C, H, W)
-                out = ddt(z_t, t, uncond)
 
-            if one_batch:
-                ## Test run
-                break
+                ddt(z_t, t, uncond)
 
+                if one_batch:
+                    break
 
 
 if __name__ == "__main__":
 
     CONF_PATH = "configs/repa_improved_ddt_xlen22de6_256.yaml"
     CKPT_PATH = "model.ckpt"
-    
     device = "cuda" if torch.cuda.is_available() else "cpu"
     print(f"Device {device.upper()}")
+
+    
+    activations = []
 
     model = DDTWrapper(
         config_path=CONF_PATH,
         ckpt_path=CKPT_PATH,
         device=device
     )
-
-    activations = []
     model.register_encoder_hook(activations)
     
 
